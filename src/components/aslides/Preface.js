3f617f3a7a1c635fe796608bc78c592987b8ff7b
vaguebrownfox <foxjournal.nsfw@gmail.com>
2022-01-13 15:54:14 +0530

    slides

--- src/components/aslides/Preface.js
+++ src/components/aslides/Preface.js
@@ -1,15 +1,49 @@
-import { Typography } from "@mui/material";
 import React from "react";
-import Basic from "../viz/Basic";
+import * as d3 from "d3";
+import { Typography } from "@mui/material";
 import classes from "../../styles/SlidePanel.module.css";
+import PanVizLayout from "../viz/PanVizLayout";
 
 const Preface = () => {
+	const [slideCount, setSlideCount] = React.useState(0);
+
+	const handleSlideNext = () => {
+		setSlideCount((p) => p + 1);
+	};
+
+	const handleSlidePrev = () => {
+		setSlideCount((p) => (p - 1 < 0 ? 0 : p - 1));
+	};
+
+	async function drawPreface(svgRef) {
+		const height = svgRef.current.height.baseVal.value;
+		const width = svgRef.current.width.baseVal.value;
+
+		const svg = d3.select(svgRef.current);
+		svg.selectAll("*").remove();
+
+		console.log(svgRef.current.width.baseVal.value);
+
+		svg.append("text")
+			.attr("text-anchor", "middle")
+			.attr("x", "50%")
+			.attr("y", "50%")
+			.text("preface" + this.slideCount);
+	}
+
 	return (
 		<div className={classes.root}>
 			<Typography variant="h3" textAlign="center">
 				Preface
 			</Typography>
-			<Basic />
+			<PanVizLayout
+				{...{
+					draw: drawPreface.bind({ slideCount }),
+					slideCount,
+					handleSlideNext,
+					handleSlidePrev,
+				}}
+			/>
 		</div>
 	);
 };
