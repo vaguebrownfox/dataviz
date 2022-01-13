e13ba5e267d83133f1abc58b7278173e66f7cfd2
vaguebrownfox <foxjournal.nsfw@gmail.com>
2022-01-13 17:20:01 +0530

    optm

--- src/libx/slides.js
+++ src/libx/slides.js
@@ -13,27 +13,27 @@ export function getAllSlideIds() {
 	//   }
 	// ]
 
-	// const keys = [1, 2, 3];
+	const keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-	// const sids = keys.map((_, i) => ({ params: { sid: `${i + 1}` } }));
+	const sids = keys.map((s, _) => ({ params: { sid: `${s}` } }));
 
-	const sids = [
-		{
-			params: {
-				sid: "1",
-			},
-		},
-		{
-			params: {
-				sid: "2",
-			},
-		},
-		{
-			params: {
-				sid: "3",
-			},
-		},
-	];
+	// const sids = [
+	// 	{
+	// 		params: {
+	// 			sid: "1",
+	// 		},
+	// 	},
+	// 	{
+	// 		params: {
+	// 			sid: "2",
+	// 		},
+	// 	},
+	// 	{
+	// 		params: {
+	// 			sid: "3",
+	// 		},
+	// 	},
+	// ];
 
 	return sids;
 }
